f9f0f1284cbfbba017bba73af421ecc54547134e
Adesz05 <kerekadamtamas@turr.hu>
2022-09-14 10:32:16 +0200

    Semmi sincs kesz

--- 2045/Form1.cs
+++ 2045/Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace _2045
 {
+    
+
     public partial class Form1 : Form
     {
+        static int szorzo = 4;
+        static int[,] matrix = new int[szorzo, szorzo];
+        static System.Windows.Forms.Label[,] labelmatrix = new System.Windows.Forms.Label[szorzo, szorzo];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button1.Visible = false;
+            
         }
     }
 }
